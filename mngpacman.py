9d1f1520e6edd311bdbf4b1ba90b9e86475b7f02
ReanGD <reangd@yandex.ru>
2015-05-30 23:27:37 +0300

    обернул все в классы

--- mngpacman.py
+++ mngpacman.py
@@ -1,38 +1,71 @@
 #!/usr/bin/env python
 import os
+import platform
 import subprocess
 
 
-def parse_cmd(cmd):
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    out, err = p.communicate()
-    return set(out.decode("UTF-8").split())
+class PackageManager(object):
+    def all_packages(self):
+        raise NotImplementedError()
+
+    def explicit_packages(self):
+        raise NotImplementedError()
+
+    def depend_packages(self, package):
+        raise NotImplementedError()
+
+    def group_depend_packages(self, group):
+        raise NotImplementedError()
 
 
-def all_packages():
-    return parse_cmd(["pacman", "-Qq"])
+class PacmanManager(PackageManager):
+    def __parse(self, cmd):
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        out, err = p.communicate()
+        return set(out.decode("UTF-8").split())
+
+    def all_packages(self):
+        return self.__parse(["pacman", "-Qq"])
+
+    def explicit_packages(self):
+        return self.__parse(["pacman", "-Qeq"])
+
+    def depend_packages(self, package):
+        l = self.__parse(["pactree", "-u", package])
+        l.discard(package)
+        return l
+
+    def groups_depend_packages(self, groups):
+        pkgs = set()
+        for it in groups:
+            pkgs.update(self.__parse(["pacman", "-Qgq", it]))
+        return pkgs
 
 
-def explicit_packages():
-    return parse_cmd(["pacman", "-Qeq"])
+def get_package_manager():
+    platform_name = platform.linux_distribution()[0]
+    if platform_name == "arch":
+        return PacmanManager()
+    else:
+        raise NotImplementedError()
 
 
-def all_depends(package):
-    l = parse_cmd(["pactree", "-u", package])
-    l.discard(package)
-    return l
+class Config(object):
+    def __init__(self, path):
+        if not os.path.isfile(path):
+            path = os.path.join(os.getcwd(), path)
+        config_data = {}
+        exec(open(path).read(), config_data)
+        self.packages = set(config_data["packages"])
+        self.ignore_groups = set(config_data["ignore_groups"])
 
 
-def load_config(config_path):
-    if not os.path.isfile(config_path):
-        config_path = os.path.join(os.getcwd(), config_path)
-    config_data = {}
-    exec(open(config_path).read(), config_data)
-    return set(config_data["pkgs"])
+mng = get_package_manager()
+cfg = Config("config.py")
 
-config_pkgs = load_config("config.py")
-explicit_pkgs = explicit_packages()
-print(len(explicit_pkgs))
-new_pkgs = explicit_pkgs.difference(config_pkgs)
+ignore_pkgs = mng.groups_depend_packages(cfg.ignore_groups)
+explicit_pkgs = mng.explicit_packages()
+new_pkgs = explicit_pkgs.difference(ignore_pkgs, cfg.packages)
 print(len(new_pkgs))
 print(new_pkgs)
+print(len(cfg.packages.intersection(ignore_pkgs)))
