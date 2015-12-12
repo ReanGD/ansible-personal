7354e449b6e0cb3a2c776875f7e05953b3d94727
ReanGD <reangd@yandex.ru>
2015-12-13 01:12:05 +0300

    add update task

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/ansible-playbook -i localhost, main.yml --ask-sudo-pass --skip-tags "view_new" $@
+/usr/bin/ansible-playbook -i localhost, main.yml --ask-sudo-pass --skip-tags "view_new,update" $@
