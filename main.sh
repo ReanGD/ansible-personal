bd701c190946b3231c40a807ea76092dd9f751db
ReanGD <reangd@yandex.ru>
2017-03-23 00:59:52 +0300

    ключи для github

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-/usr/bin/ansible-playbook -i localhost, main.yml --ask-sudo-pass --skip-tags "view_new,update" $@
+/usr/bin/ansible-playbook -i localhost, main.yml --ask-sudo-pass --ask-vault-pass --skip-tags "view_new,update" $@
