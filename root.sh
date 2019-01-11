4131bf9a0af190e82696b00b60191d4e2446f180
ReanGD <reangd@yandex.ru>
2019-01-11 22:52:04 +0300

    add boot role

--- root.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash	
-
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-cd $ROOT_DIR
-/usr/bin/ansible-playbook tasks/common/main_root.yml --ask-become-pass --skip-tags "view_new,update" $@
