77571ea2c2aea0d7d53acc0e727c437ef38b3b5a
ReanGD <reangd@yandex.ru>
2019-01-25 03:29:56 +0300

    add packages role

---
+++ roles/packages/vars/packages_archsrv.py
@@ -0,0 +1,34 @@
+# global host
+pkgs = []
+grps = []
+
+# monitoring
+pkgs += ["iftop",  # network monitor
+         "htop",  # process monitor
+         "iotop",  # disk monitor
+         "hddtemp",  # disk temperature
+         "smartmontools"]
+
+# system
+pkgs += ["refind-efi",
+         "gnupg",
+         "pkgfile",  # pkgfile makepkg (get package for makepkg)
+         "pkgcacheclean",  # clean the pacman cache         
+         "aurman",  # AUR package manager
+         "rsync"]
+
+# net tools
+pkgs += ["wget"]
+
+# python
+pkgs += ["python"]
+# git
+pkgs += ["git"]
+
+# VM
+pkgs += ["docker", "docker-compose"]
+
+packages = pkgs
+groups = grps
+ignore_packages = ["yaourt", "ansible"]
+ignore_groups = ["base", "base-devel"]
