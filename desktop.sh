e0a19361df0e9588c03fc4bdd114b7afaa6f4bba
ReanGD <reangd@yandex.ru>
2019-01-30 03:29:56 +0300

    add role go_env

--- desktop.sh
+++ desktop.sh
@@ -3,4 +3,4 @@
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cd $ROOT_DIR
-/usr/bin/ansible-playbook local_update.yml --ask-become-pass --ask-vault-pass $@
+/usr/bin/ansible-playbook desktop.yml --ask-become-pass --ask-vault-pass $@
