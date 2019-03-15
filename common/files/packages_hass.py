00f7889455530efc366b98e6197f58ebdad42088
ReanGD <reangd@yandex.ru>
2019-03-16 01:22:00 +0300

    packages for hass

--- common/files/packages_hass.py
+++ common/files/packages_hass.py
@@ -3,8 +3,31 @@ pkgs = []
 grps = []
 
 # system
-pkgs += ["pacman",
-         "rsync"]
+pkgs += ["raspberrypi-firmware", "raspberrypi-bootloader-x", "linux-raspberrypi", "raspberrypi-bootloader"]
+
+# monitoring
+pkgs += ["rsync",
+         "gnupg",
+         "iftop",  # network monitor
+         "htop",  # process monitor
+         "iotop",  # disk monitor
+         "hddtemp",  # disk temperature
+         "smartmontools"]
+
+# net tools
+pkgs += ["net-tools", "openssh"]
+
+# python
+pkgs += ["python", "python-pip", "python-virtualenv"]
+
+# build tools
+pkgs += ["libffi", "libudev0-shim"]
+
+# hass deps
+pkgs += ["protobuf", "mosquitto"]
+
+# groups
+grps += ["base", "base-devel"]
 
 packages = pkgs
 groups = grps
