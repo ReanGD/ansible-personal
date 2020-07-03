a0037bbfb5a878c9e7ab7d4fa5edb8957eca2d41
ReanGD <reangd@yandex.ru>
2020-07-03 20:18:01 +0300

    fix install yay packages

--- library/pkg_manager.py
+++ library/pkg_manager.py
@@ -118,7 +118,7 @@ class InstallManager:
         if manager == "pacman":
             params = ["env", "LC_ALL=C", "sudo", "pacman", "-S", "--noconfirm"]
         else:
-            params = ["env", "LC_ALL=C", manager, "-S", "--answerclean", "All", "--answerdiff", "None", "--answeredit", "None", "--nopgpfetch", "--noconfirm"]
+            params = ["env", "LC_ALL=C", manager, "-S", "--answerclean", "All", "--answerdiff", "None", "--answeredit", "None", "--nopgpfetch", "--noconfirm", "--mflags", "--skippgpcheck"]
 
         if as_explicit is not None:
             if as_explicit:
