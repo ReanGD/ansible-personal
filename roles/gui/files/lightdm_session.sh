559e152c8affed211b7bfb2f31c3c4498235240a
ReanGD <reangd@yandex.ru>
2020-08-05 01:56:03 +0300

    fix errors with load session

--- roles/gui/files/lightdm_session.sh
+++ roles/gui/files/lightdm_session.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Run standart session wrapper
-. /etc/lightdm/Xsession
+. /etc/lightdm/Xsession ""
 
 if [ -n "$HOME" ] && [ -r "$HOME/.config/start/startx_profile.sh" ]; then
 . "$HOME/.config/start/startx_profile.sh"
