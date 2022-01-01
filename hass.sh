3d1cf152313099329e062cd887e1a56b92d6da71
ReanGD <reangd@yandex.ru>
2022-01-01 03:50:32 +0300

    upgrade hass setup script

--- hass.sh
+++ hass.sh
@@ -1,29 +1,24 @@
-#!/bin/bash	
+#!/bin/bash
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 MENU_ID=$1
 
-BASEURL=http://os.archlinuxarm.org/os/
-RPI2=ArchLinuxARM-rpi-2-latest.tar.gz
-RPI3=ArchLinuxARM-rpi-3-latest.tar.gz
-ARMV7IMG=arch-linux-armv7.img
-ARMV8IMG=arch-linux-armv8.img
-
 run_with_sudo(){
 	if [ $EUID != 0 ]; then
-		sudo "$0" "$MENU_ID"
+		sudo -E "$0" "$MENU_ID"
 		exit $?
 	fi
 }
 
-create_image(){	
-	NAME=$1
-	IMG=$2
+create_image(){
+	BASEURL="http://os.archlinuxarm.org/os/"
+	TAR_FILE_NAME="ArchLinuxARM-rpi-armv7-latest.tar.gz"
+	IMG_FILE_NAME="arch-linux-armv7.img"
 	cd ~/tmp
 	losetup /dev/loop0 && exit 1 || true
-	wget -N $BASEURL$NAME
-	truncate -s 1300M $IMG
-	losetup /dev/loop0 $IMG
+	wget -N $BASEURL$TAR_FILE_NAME
+	truncate -s 1500M $IMG_FILE_NAME
+	losetup /dev/loop0 $IMG_FILE_NAME
 	parted -s /dev/loop0 mklabel msdos
 	parted -s /dev/loop0 unit MiB mkpart primary fat32 -- 1 100
 	parted -s /dev/loop0 set 1 boot on
@@ -35,16 +30,15 @@ create_image(){
 	mount /dev/loop0p1 arch-boot
 	mkdir -p arch-root
 	mount /dev/loop0p2 arch-root
-	bsdtar -xpf $NAME -C arch-root
+	bsdtar -xpf $TAR_FILE_NAME -C arch-root
+
 	mv arch-root/boot/* arch-boot/
-	
+
 	# fix boot
 	sed -i 's/gpu_mem=64/gpu_mem=16/' arch-boot/config.txt
 	sed -i "s/ defaults / defaults,noatime /" arch-root/etc/fstab
 	sed -i "s/mmcblk0p1/sda1/" arch-root/etc/fstab
-	if [ $NAME = $RPI2 ]; then
-		sed -i "s/mmcblk0p2/sda2/" arch-boot/cmdline.txt
-	fi
+	sed -i "s/mmcblk0p2/sda2/" arch-boot/cmdline.txt
 
 	# ssh config
 	echo 'PermitRootLogin yes' > arch-root/etc/ssh/sshd_config
@@ -64,18 +58,17 @@ create_image(){
 	umount arch-boot arch-root
 	rm -rf arch-boot arch-root
 	losetup -d /dev/loop0
-	echo "Finish. Image path = ~/tmp/$IMG"
+	echo "Finish. Image path = ~/tmp/$IMG_FILE_NAME"
 }
 
 main_menu(){
 	MENU_ID=$(whiptail --clear --title 'Scenarios for raspberry Pi 3'\
 	--menu "Enter your choice:" 15 60 6 \
-		"1" "Create image for ARMv7" \
-		"2" "Create image for ARMv8" \
+		"1" "Ansible update" \
+		"2" "Ansible copy settings to local" \
 		"3" "Ansible base install" \
-		"4" "Ansible update" \
-		"5" "Ansible copy settings to local" \
-		"6" "Quit" \
+		"4" "Create image for ARMv7" \
+		"5" "Quit" \
 		3>&1 1>&2 2>&3)
 
 	if [ $? != 0 ]; then
@@ -89,26 +82,22 @@ fi
 
 case $MENU_ID in
   "1")
-	run_with_sudo
-	create_image "$RPI2" "$ARMV7IMG"
+	cd $ROOT_DIR
+	/usr/bin/ansible-playbook hass_update.yml --ask-become-pass --ask-vault-pass
 	;;
   "2")
-	run_with_sudo
-	create_image "$RPI3" "$ARMV8IMG"
+	cd $ROOT_DIR
+	/usr/bin/ansible-playbook hass_copy.yml
 	;;
   "3")
 	cd $ROOT_DIR
 	/usr/bin/ansible-playbook hass_install.yml --ask-become-pass --ask-vault-pass
 	;;
   "4")
-	cd $ROOT_DIR
-	/usr/bin/ansible-playbook hass_update.yml --ask-become-pass --ask-vault-pass
+	run_with_sudo
+	create_image
 	;;
   "5")
-	cd $ROOT_DIR
-	/usr/bin/ansible-playbook hass_copy.yml
-	;;	
-  "6")
 	exit 1
 	;;
 esac
