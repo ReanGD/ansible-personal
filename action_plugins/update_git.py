f616b546b38ff8abdcfde1a3c66ba5083be45bf5
ReanGD <reangd@yandex.ru>
2025-04-29 10:31:09 +0300

    update git plugins

--- action_plugins/update_git.py
+++ action_plugins/update_git.py
@@ -1,98 +1,40 @@
-from traceback import format_exc
+from __future__ import division, print_function, absolute_import
+
+__metaclass__ = type
+
+from ansible.utils.display import Display
 from ansible.plugins.action import ActionBase
 
-try:
-    from __main__ import display
-except ImportError:
-    from ansible.utils.display import Display
-    display = Display()
-
-
-class Clr:
-    COLOR_HIGHLIGHT = 'white'
-    COLOR_VERBOSE = 'blue'
-    COLOR_WARN = 'bright purple'
-    COLOR_ERROR = 'red'
-    COLOR_DEBUG = 'dark gray'
-    COLOR_DEPRECATE = 'purple'
-    COLOR_SKIP = 'cyan'
-    COLOR_UNREACHABLE = 'bright red'
-    COLOR_OK = 'green'
-    COLOR_CHANGED = 'yellow'
-
-
-class StrError(RuntimeError):
-    pass
+display = Display()
 
 
 class ActionModule(ActionBase):
     TRANSFERS_FILES = False
 
-    def _get_param_dest(self):
-        param_name = "dest"
-        result = self._task.args.get(param_name, None)
-        if result is None or result.strip() == "":
-            raise StrError("Not found required param '{}'.".format(param_name))
-
-        return result.strip()
-
-    def _get_param_repo(self):
-        param_name = "repo"
-        result = self._task.args.get(param_name, None)
-        if result is None or result.strip() == "":
-            raise StrError("Not found required param '{}'.".format(param_name))
-
-        return result.strip()
-
-    @staticmethod
-    def _print_exception(text):
-        display.error(text, wrap_text=False)
-
-    @staticmethod
-    def _print_warning(text):
-        display.display(text, color=Clr.COLOR_WARN)
-
-    def _call_module(self, name, args):
-        result = self._execute_module(module_name=name, module_args=args,
-                                      task_vars=self._task_vars)
-        if result.get("failed"):
-            exception = result.get("exception", None)
-            if exception is not None:
-                self._print_exception(exception)
-
-            msg = result.get("msg", "Unknown error in module {}".format(name))
-            raise StrError("lib error: {}".format(msg))
-
-        return result
-
-    def _run(self):
-        args = {"dest": self._get_param_dest(), "repo": self._get_param_repo()}
-        result = self._call_module(name="update_git", args=args)
-
-        show_warning = result.get("show_warning")
-        if show_warning is not None:
-            self._print_warning(show_warning)
-
-        return result
-
     def run(self, tmp=None, task_vars=None):
         if task_vars is None:
             task_vars = dict()
 
-        self._supports_async = False
-        self._supports_check_mode = False
         result = super(ActionModule, self).run(tmp, task_vars)
-        del tmp  # tmp no longer has any effect
 
-        self._task_vars = task_vars
-        try:
-            result.update(self._run())
-        except StrError as e:
-            result['failed'] = True
-            result['msg'] = str(e)
-        except Exception:
-            ActionModule._print_exception(format_exc())
-            result['failed'] = True
-            result['msg'] = "Error in action_plugin/update_git: "
+        repo = self._task.args.get("repo")
+        dest = self._task.args.get("dest")
 
+        if not repo or not dest:
+            result["failed"] = True
+            result["msg"] = "Missing required arguments: repo and/or dest"
+            return result
+
+        module_return = self._execute_module(
+            module_name="update_git", module_args={"repo": repo, "dest": dest}, task_vars=task_vars
+        )
+
+        if module_return.get("failed"):
+            return module_return
+
+        show_warning = module_return.get("show_warning")
+        if show_warning:
+            display.warning(show_warning)
+
+        result.update(module_return)
         return result
--- library/update_git.py
+++ library/update_git.py
@@ -1,150 +1,100 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import os
-import re
-from traceback import format_exc
+
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils._text import to_native
+from ansible.module_utils.common.text.converters import to_native
 
 
-class StrError(RuntimeError):
-    pass
-
-
-def head_splitter(headfile, module):
-    res = None
-    if os.path.exists(headfile):
-        rawdata = None
-        try:
-            f = open(headfile, 'r')
-            rawdata = f.readline()
-            f.close()
-        except:
-            raise StrError("Unable to read '{}'".format(headfile))
-        if rawdata:
-            try:
-                rawdata = rawdata.replace("refs/remotes/origin", "", 1)
-                refparts = rawdata.split(" ")
-                newref = refparts[-1]
-                nrefparts = newref.split("/", 2)
-                res = nrefparts[-1].rstrip("\n")
-            except:
-                raise StrError("Unable to split head from '{}'".format(rawdata))
-    return res
-
-
-def get_version(module, git_path, dest):
+def get_version(module, git_path, dest) -> None | str:
     cmd = [git_path, "rev-parse", "HEAD"]
     rc, stdout, stderr = module.run_command(cmd, cwd=dest)
-    sha = to_native(stdout).rstrip('\n')
-    return sha
+    if rc != 0:
+        return None
+    return to_native(stdout).strip()
 
 
-def clone(git_path, module, repo, dest):
+def clone(module, git_path, repo, dest) -> None:
     dest_dirname = os.path.dirname(dest)
-    try:
-        os.makedirs(dest_dirname)
-    except:
-        pass
+    if not os.path.exists(dest_dirname):
+        os.makedirs(dest_dirname, exist_ok=True)
+
     cmd = [git_path, "clone", repo, dest]
     module.run_command(cmd, check_rc=True, cwd=dest_dirname)
 
 
-def has_local_mods(module, git_path, dest):
+def has_local_mods(module, git_path, dest) -> bool:
     cmd = [git_path, "status", "--porcelain"]
     rc, stdout, stderr = module.run_command(cmd, cwd=dest)
+    if rc != 0:
+        return False
     lines = stdout.splitlines()
-    lines = list(filter(lambda c: not re.search('^\\?\\?.*$', c), lines))
+    # Игнорируем untracked файлы (??)
+    filtered = [l for l in lines if not l.startswith("??")]
+    return len(filtered) > 0
 
-    return len(lines) > 0
 
-
-def get_branches(git_path, module, dest):
-    branches = []
-    cmd = [git_path, "branch", "--no-color", "-a"]
-    (rc, out, err) = module.run_command(cmd, cwd=dest)
+def get_head_branch(module, git_path, dest) -> str:
+    cmd = [git_path, "symbolic-ref", "--short", "-q", "HEAD"]
+    rc, stdout, stderr = module.run_command(cmd, cwd=dest)
     if rc != 0:
-        raise StrError("Could not determine branch data, stdout: {}, stderr: {}, rc: {}".format(out, err, rc))
-    for line in out.split('\n'):
-        if line.strip():
-            branches.append(line.strip())
-    return branches
+        return "_not_a_branch"  # Detached HEAD
+    return to_native(stdout).strip()
 
 
-def is_not_a_branch(git_path, module, dest):
-    branches = get_branches(git_path, module, dest)
-    for branch in branches:
-        if branch.startswith("* ") and ("no branch" in branch or
-                                        "detached from" in branch or
-                                        "detached at" in branch):
-            return True
-    return False
-
-
-def get_head_branch(git_path, module, dest):
-    if is_not_a_branch(git_path, module, dest):
-        return "_not_a_branch"
-    headfile = os.path.join(dest, ".git", "HEAD")
-    branch = head_splitter(headfile, module)
-    return branch
-
-
-def pull_master(git_path, module, dest):
+def pull_master(module, git_path, dest) -> bool:
     version_before = get_version(module, git_path, dest)
-    cmd = [git_path, 'pull', 'origin', 'master']
-    (rc, out, err) = module.run_command(cmd, cwd=dest)
+    cmd = [git_path, "pull", "origin", "master"]
+    rc, stdout, stderr = module.run_command(cmd, cwd=dest)
+
     if rc != 0:
-        raise StrError("Failed to pull branch master, stdout: {}, stderr: {}, rc: {}".format(out, err, rc))
+        module.fail_json(msg=f"Failed to pull master: {stderr}")
 
-    return version_before != get_version(module, git_path, dest)
-
-
-def run_module(module):
-    dest = os.path.abspath(module.params['dest'])
-    repo = module.params['repo']
-    git_path = module.get_bin_path('git', True)
-    is_dest = os.path.exists(dest)
-    result = {"changed": False}
-
-    module.run_command_environ_update = dict(LANG='C', LC_ALL='C', LC_MESSAGES='C', LC_CTYPE='C')
-
-    if is_dest and not os.path.exists(os.path.join(dest, ".git", 'config')):
-        raise StrError("Dest directory '{}', is not empty".format(dest))
-    elif not is_dest:
-        clone(git_path, module, repo, dest)
-        result.update(changed=True)
-    elif has_local_mods(module, git_path, dest):
-        result.update(show_warning="Local modifications exist in repository ({})".format(dest))
-    elif get_head_branch(git_path, module, dest) != 'master':
-        result.update(show_warning="HEAD branch not a master in repository ({})".format(dest))
-    elif pull_master(git_path, module, dest):
-        result.update(changed=True)
-
-    return result
+    version_after = get_version(module, git_path, dest)
+    return version_before != version_after
 
 
-# update_git: repo=git@github.com:User/Repo dest=/home/repodir
-def main():
+def run_module() -> None:
     module = AnsibleModule(
         argument_spec=dict(
-            dest=dict(default=None, required=True, type="path"),
-            repo=dict(default=None, required=True, type="str"),
-            ),
-        supports_check_mode=False
+            dest=dict(required=True, type="path"),
+            repo=dict(required=True, type="str"),
+        ),
+        supports_check_mode=False,
     )
 
-    result = {}
+    dest = os.path.abspath(module.params["dest"])
+    repo = module.params["repo"]
+    git_path = module.get_bin_path("git", True)
+
+    module.run_command_environ_update = dict(LANG="C", LC_ALL="C", LC_MESSAGES="C")
+
+    result = {"changed": False}
+
     try:
-        result = run_module(module)
-    except StrError as e:
-        result.update(failed=True, msg="Error in library/update_git: !!!")
-        # result.update(failed=True, msg="Error in library/update_git: " + str(e))
-        # module.fail_json(msg="Error in library/update_git: " + str(e))
-    except Exception:
-        result.update(failed=True, msg="Error in library/update_git: !!!")
-        # result.update(failed=True, msg="Exception in library/update_git", exception=format_exc())
-        # module.fail_json(msg="Exception in library/update_git", exception=format_exc())
+        if not os.path.exists(dest):
+            clone(module, git_path, repo, dest)
+            result["changed"] = True
+        else:
+            if not os.path.exists(os.path.join(dest, ".git")):
+                module.fail_json(
+                    msg=f"Destination directory '{dest}' exists and is not a git repository"
+                )
 
-    module.exit_json(**result)
+            if has_local_mods(module, git_path, dest):
+                result["show_warning"] = f"Local modifications exist in repository ({dest})"
+            elif get_head_branch(module, git_path, dest) != "master":
+                result["show_warning"] = f"HEAD branch is not 'master' in repository ({dest})"
+            else:
+                if pull_master(module, git_path, dest):
+                    result["changed"] = True
+
+        module.exit_json(**result)
+
+    except Exception as e:
+        module.fail_json(msg=f"Unexpected error: {str(e)}")
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    run_module()
